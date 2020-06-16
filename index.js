f3f896bc60154aa7fc1b256c51da5a704a6077d5
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-15 20:48:28 -0400

    added prompts to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-const questions = [
+const questions = [{
+    type: "text",
+    name: "title",
+    message: "What is the title of your project?",
+},
+{
+    type: "text",
+    name: "description",
+    message: "Provide breif description for your project", 
+},
+{
+    type: "text",
+    name: "table of contents",
+    message: "Provide a table of contents for your project",
+},
+{
+    type: "text",
+    name: "installation",
+    message: "How will you be installing?",
+},
+{
+    type: "text",
+    name: "usage",
+    message: "Please provide instructions for use",
+},
+{
+    type: "text",
+    name: "license",
+    message: "List any liscening for your project",
+},
+{
+    type: "text",
+    name: "contributes",
+    message: "List any collaborators, if any?",
+},
+{
+    type: "text",
+    name: "tests",
+    message: "Provide how to run test of your project",
+},
+{
+    type: "text",
+    name: "title",
+    message: "What is the title of your project?",
+}
 
 ];
 
