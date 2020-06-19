065cbfe0f38eccbc731f8f1331e6f179579b8e71
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-19 10:32:52 -0400

    added more html for readme styling

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const questions = [{
 
 (async () => {
     const response = await prompts(questions); 
-    let readMeList = `<h1>Title</h1>` +"\n" + "\n" + `${response.title}` +"\n" + "\n" + `Description` + "\n" + "\n" + `${response.description}` + "\n" + "\n" + `${response.content}` + "\n" + response.installation + "\n" + response.usage +"\n" + response.license +"\n" + response.contributes +"\n" + response.badge +"\n" + response.questions
+    let readMeList = `<h1>Title</h1>` +"\n" +  `${response.title}` +"\n" + "\n" + `<h1>Description</h1>` + "\n" + `${response.description}` + "\n" + "\n" + `<h1>Table of Contents</h1>`+ "\n" + `<ul> <a>${response.content}</a> </ul>` + "\n" + "\n" + `<h1>Installation</h1>` +"\n" + `${response.installation}` + "\n" + response.usage +"\n" + response.license +"\n" + response.contributes +"\n" + response.badge +"\n" + response.questions
     
     fs.writeFile('README.md', readMeList, function (err){
     if(err) throw err;
